be0d3f49d616e752574fc09b505ca82fc68c26bb
Leonardo Jerez <leo_220890@hotmail.com>
2024-01-18 02:08:58 -0300

    update

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -2,12 +2,22 @@
 var fs = require("fs");
 const utils = require("./utils");
 
-
 const { Builder, Browser, Capabilities } = require("selenium-webdriver");
 const { Before, After } = require("@cucumber/cucumber");
 
 Before(async () => {
-  global.driver = new Builder().forBrowser(Browser.CHROME).build();
+  if (process.env.GIT == true) {
+    const host = process.env.SELENIUM || "selenium";
+    const browser = process.env.BROWSER
+    const server = `http://${host}:4444/`;
+    global.driver = new Builder()
+      .usingServer(server)
+      .forBrowser(Browser.browser)
+      .build();
+  } else {
+    global.driver = new Builder().forBrowser(Browser.CHROME).build();
+    
+  }
   driver.manage().window().maximize();
 });
 
